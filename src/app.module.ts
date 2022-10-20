1de9333f930cda52e6772f501b5c15b07940ae44
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-20 05:07:18 -0300

    feat: configuring mongoose into module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule,
+    MongooseModule.forRoot(process.env.MONGO_DSN),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
